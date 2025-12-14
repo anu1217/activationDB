ddae43c428591a612ba69950713515eb73cc3916
Anupama Rajendra <asrajendra@wisc.edu>
2025-12-14 15:52:58 -0600

    add template script

---
+++ tools/script_template.py
@@ -0,0 +1,29 @@
+import alara_output_processing as aop
+import numpy as np
+import openmc
+import argparse
+import yaml
+import pandas as pd
+import sqlite3
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db_yaml', default = "iter_dt_out.yaml", help="Path (str) to YAML containing inputs")
+    args = parser.parse_args()
+    return args
+
+def read_yaml(yaml_arg):
+    '''
+    input:
+        yaml_arg : output of parse_args() corresponding to args.db_yaml
+    '''
+    with open(yaml_arg, 'r') as yaml_file:
+        inputs = yaml.safe_load(yaml_file)
+    return inputs
+
+def main():        
+    args = parse_args()
+    inputs = read_yaml(args.db_yaml)  
+
+if __name__ == "__main__":
+    main()    

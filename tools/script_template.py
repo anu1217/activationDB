bb43f17291c78a0b74a15a908355bffc2a1b725c
Anupama Rajendra <asrajendra@wisc.edu>
2026-02-09 17:27:17 -0600

    use values from parsing output

--- tools/script_template.py
+++ tools/script_template.py
@@ -3,27 +3,51 @@ import yaml
 import numpy as np
 import openmc
 import json
+import sys
+import os
+sys.path.insert(0,'../../ALARA_fork/ALARA/tools')
+import sched_post_processor as spp
 
-def calc_time_params_from_out(sch_dict)
-
-def calc_time_params(active_burn_time, duty_cycle_list, num_pulses):
+def search_for_match(top_dict, search_str):
     '''
-    Uses provided pulsing information to determine dwell time and total irradiation time.
-    Assumes that the active irradiation time per pulse and dwell time between pulses both remain constant in any given simulation.
-    (This is always maintained for a single-level schedule and single-level pulse history.)
-
-    Iterates over the number of pulses, and for each number, calculates dwell time.
-    The duty cycle is defined as the pulse length / (pulse length + dwell time).
-    inputs:
-        active_burn_time : total active irradiation time (float) in any chosen unit
-        duty_cycle_list : list of chosen duty cycles (float)
-        num_pulses : list of number of pulses (int) that the active irradiation period is divided into
+    Takes a nested top dictionary and searches all sub-dictionaries for matches with a provided string.
     '''
-    pulse_lengths = active_burn_time / num_pulses
-    rel_dwell_times = (1 - duty_cycle_list) / duty_cycle_list
-    abs_dwell_times = np.outer(rel_dwell_times, pulse_lengths)
+    matches = []
+    for key, value in top_dict.items():
+
+        if key.startswith(search_str):
+            matches.append(value)
+
+        elif isinstance(value, dict):
+            match_res = search_for_match(value, search_str)
+            matches.extend(match_res)
+
+    return matches
+
+def calc_time_params_from_out(run_dicts):
+    num_pulses = []
+    abs_dwell_times = []
+    for runs_dicts_idx, (dt_group, pulse_kv) in enumerate(run_dicts.items()):
+        for run_idx, (run_lbl, out_path) in enumerate(pulse_kv.items()):
+            lines = spp.read_out(out_path)
+            pulse_dict = spp.read_pulse_histories(lines)
+            sch_dict = spp.make_nested_dict(lines)
+
+            n_pulses = eval(search_for_match(pulse_dict, 'num_pulses_all_levels')[0].strip(" []"))
+            num_pulses.append(n_pulses)
+            
+            pulse_length = search_for_match(sch_dict, 'pe_dur')[0]
+
+            abs_dwell_time = eval(search_for_match(pulse_dict, 'delay_seconds_all_levels')[0].strip(" []"))
+            abs_dwell_times.append(abs_dwell_time)
+            
+    # by construction, the active burn time is the same for all runs         
+    active_burn_time = pulse_length * n_pulses        
+    num_pulses = np.asarray(list(dict.fromkeys(num_pulses)))
+    abs_dwell_times = np.array(abs_dwell_times).reshape(len(run_dicts.items()), len(pulse_kv.items()))
     t_irr_arr = active_burn_time + abs_dwell_times * (num_pulses - 1)
-    return pulse_lengths, abs_dwell_times, t_irr_arr
+    return pulse_dict, sch_dict, t_irr_arr, active_burn_time
+
 
 def open_flux_file(flux_file):
     with open(flux_file, 'r') as flux_data:
@@ -38,7 +62,7 @@ def parse_flux_lines(flux_lines):
     input : flux_lines (list of lines from ALARA flux file)
     output : flux_array (numpy array of shape # intervals x number of energy groups)
     '''
-    energy_bins = openmc.mgxs.GROUP_STRUCTURES['VITAMIN-J-175']           
+    energy_bins = openmc.mgxs.GROUP_STRUCTURES['VITAMIN-J-175']
     all_entries = np.array(' '.join(flux_lines).split(), dtype=float)
     if len(all_entries) == 0:
         raise Exception("The chosen flux file is empty.")
@@ -64,32 +88,23 @@ def read_yaml(yaml_arg):
         inputs = yaml.safe_load(yaml_file)
     return inputs
 
-def read_json(json_filename):
-    with open(json_filename, 'r') as json_file:
-        sch_dict = json.load(json_file)
-    return sch_dict    
-
-def main():        
+def main():
     args = parse_args()
     inputs = read_yaml(args.db_yaml)
 
-    json_filename = inputs['json_filename']
-    sch_dict = read_json(json_filename)
-
-    flux_file = inputs['flux_file'] 
+    flux_file = inputs['flux_file']
+    run_dicts = inputs['run_dicts']
     flux_lines = open_flux_file(flux_file)
     flux_array = parse_flux_lines(flux_lines)
 
-    active_burn_time = np.asarray(inputs['active_burn_time'])
-    duty_cycle_list = np.asarray(inputs['duty_cycles'])
-    num_pulses = np.asarray(inputs['num_pulses'])
-    pulse_lengths, abs_dwell_times, t_irr_arr = calc_time_params(active_burn_time, duty_cycle_list, num_pulses)
+    pulse_dict, sch_dict, t_irr_arr, active_burn_time = calc_time_params_from_out(run_dicts)
 
     total_flux = np.sum(flux_array, axis=1) #sum over the bin widths of flux array
     # normalize flux spectrum by the total flux in each interval
     norm_flux_arr =  flux_array / total_flux.reshape(len(total_flux), 1) # 2D array of shape num_intervals x num_groups
     # for each interval, calculate flux averaged over time elapsed between the start of the 1st pulse and the end of the last
     avg_flux_arr = np.multiply.outer(total_flux, active_burn_time / t_irr_arr) # array of shape num_intervals x len(duty_cycles) x len(num_pulses)
+
 
 if __name__ == "__main__":
     main()

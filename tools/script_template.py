8e7a33e9a0d022dc91319e3db0e7342faef3cfa7
Anupama Rajendra <asrajendra@wisc.edu>
2026-02-11 12:07:54 -0600

    Revert "rewrite function to calculate t_irr_arr"
    This reverts commit 6f65b2de015173a7c491d85798f6f2215086f062.

--- tools/script_template.py
+++ tools/script_template.py
@@ -5,7 +5,8 @@ import openmc
 import json
 import sys
 import os
-
+sys.path.insert(0,'../../ALARA_fork/ALARA/tools')
+import sched_post_processor as spp
 
 def search_for_match(top_dict, search_str):
     '''
@@ -23,45 +24,35 @@ def search_for_match(top_dict, search_str):
 
     return matches
 
-
-def calc_time_params_from_out(spp, run_dicts):
-    '''
-    Calculates an irradiation time array using parameters from ALARA output files.
-    It is assumed that the active burn time is constant over all runs.
-    input: 
-        run_dicts : dictionary containing sub-dictionaries organized by duty cycle, whose values are
-        dictionaries with keys = run labels organized by number of pulses, and values = paths to the
-        corresponding output file.
-    outputs:
-        active_burn_time: total amount of active irradiation time in seconds (float)
-        t_irr_arr : array of total irradiation times of shape (# duty cycles x # pulses)
-    '''
-    t_irr_arr = []
-    for run_dict in run_dicts.values():
-        for out_path in run_dict.values():
+def calc_time_params_from_out(run_dicts):
+    num_pulses = []
+    abs_dwell_times = []
+    for runs_dicts_idx, (dt_group, pulse_kv) in enumerate(run_dicts.items()):
+        for run_idx, (run_lbl, out_path) in enumerate(pulse_kv.items()):
             lines = spp.read_out(out_path)
             pulse_dict = spp.read_pulse_histories(lines)
             sch_dict = spp.make_nested_dict(lines)
-            num_pulses = eval(
-                search_for_match(pulse_dict,
-                                 'num_pulses_all_levels')[0].strip(" []"))
+
+            n_pulses = eval(search_for_match(pulse_dict, 'num_pulses_all_levels')[0].strip(" []"))
+            num_pulses.append(n_pulses)
+            
             pulse_length = search_for_match(sch_dict, 'pe_dur')[0]
-            abs_dwell_time = eval(
-                search_for_match(pulse_dict,
-                                 'delay_seconds_all_levels')[0].strip(" []"))
-            active_burn_time = num_pulses * pulse_length
-            t_irr_arr.append(active_burn_time + abs_dwell_time *
-                             (num_pulses - 1))
-    t_irr_arr = np.array(t_irr_arr).reshape(len(run_dicts.values()),
-                                            len(run_dict.values()))
-    return active_burn_time, t_irr_arr
+
+            abs_dwell_time = eval(search_for_match(pulse_dict, 'delay_seconds_all_levels')[0].strip(" []"))
+            abs_dwell_times.append(abs_dwell_time)
+            
+    # by construction, the active burn time is the same for all runs         
+    active_burn_time = pulse_length * n_pulses        
+    num_pulses = np.asarray(list(dict.fromkeys(num_pulses)))
+    abs_dwell_times = np.array(abs_dwell_times).reshape(len(run_dicts.items()), len(pulse_kv.items()))
+    t_irr_arr = active_burn_time + abs_dwell_times * (num_pulses - 1)
+    return pulse_dict, sch_dict, t_irr_arr, active_burn_time
 
 
 def open_flux_file(flux_file):
     with open(flux_file, 'r') as flux_data:
         flux_lines = flux_data.readlines()
     return flux_lines
-
 
 def parse_flux_lines(flux_lines):
     '''
@@ -82,20 +73,11 @@ def parse_flux_lines(flux_lines):
     flux_array = all_entries.reshape(num_intervals, num_groups)
     return flux_array
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--db_yaml',
-                        default="iter_dt_out.yaml",
-                        help="Path (str) to YAML containing inputs")
-    parser.add_argument(
-        '--spp_path',
-        '-sp',
-        required=True,
-        help="Path (str) to folder containing sched_post_processor.py")
+    parser.add_argument('--db_yaml', default = "iter_dt_out.yaml", help="Path (str) to YAML containing inputs")
     args = parser.parse_args()
     return args
-
 
 def read_yaml(yaml_arg):
     '''
@@ -106,31 +88,22 @@ def read_yaml(yaml_arg):
         inputs = yaml.safe_load(yaml_file)
     return inputs
 
-
 def main():
     args = parse_args()
     inputs = read_yaml(args.db_yaml)
-
-    spp_path = args.spp_path
-    sys.path.insert(0, spp_path)
-    import sched_post_processor as spp
 
     flux_file = inputs['flux_file']
     run_dicts = inputs['run_dicts']
     flux_lines = open_flux_file(flux_file)
     flux_array = parse_flux_lines(flux_lines)
 
-    active_burn_time, t_irr_arr = calc_time_params_from_out(spp, run_dicts)
+    pulse_dict, sch_dict, t_irr_arr, active_burn_time = calc_time_params_from_out(run_dicts)
 
-    total_flux = np.sum(flux_array,
-                        axis=1)  #sum over the bin widths of flux array
+    total_flux = np.sum(flux_array, axis=1) #sum over the bin widths of flux array
     # normalize flux spectrum by the total flux in each interval
-    norm_flux_arr = flux_array / total_flux.reshape(
-        len(total_flux), 1)  # 2D array of shape num_intervals x num_groups
+    norm_flux_arr =  flux_array / total_flux.reshape(len(total_flux), 1) # 2D array of shape num_intervals x num_groups
     # for each interval, calculate flux averaged over time elapsed between the start of the 1st pulse and the end of the last
-    avg_flux_arr = np.multiply.outer(
-        total_flux, active_burn_time / t_irr_arr
-    )  # array of shape num_intervals x len(duty_cycles) x len(num_pulses)
+    avg_flux_arr = np.multiply.outer(total_flux, active_burn_time / t_irr_arr) # array of shape num_intervals x len(duty_cycles) x len(num_pulses)
 
 
 if __name__ == "__main__":

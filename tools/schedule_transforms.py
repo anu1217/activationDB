d17dc08f594e901250ca69c8d7a080ab6c5e374a
Anupama Rajendra <113371601+anu1217@users.noreply.github.com>
2026-02-18 10:09:28 -0600

    Use level-specific ff to find total ff
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>

--- tools/schedule_transforms.py
+++ tools/schedule_transforms.py
@@ -26,10 +26,9 @@ def flatten_ph_levels(pulse_length, nums_pulses, dwell_times):
     :param nums_pulses: (iterable) number of pulses at each level
     :param dwell_times: (iterable) the duration of the gap between each pulse at each level
     '''
-    active_burn_time = nums_pulses[0] * pulse_length
-    tot_t_irr, ff = flatten_pulse_history(pulse_length, nums_pulses[0], dwell_times[0])
-    for num_pulses, dwell_time in zip(nums_pulses[1:], dwell_times[1:]):
+    tot_ff = 1
+    tot_t_irr = pulse_length
+    for num_pulses, dwell_time in zip(nums_pulses, dwell_times):
         tot_t_irr, ff = flatten_pulse_history(tot_t_irr, num_pulses, dwell_time)
-        active_burn_time = active_burn_time * num_pulses
-    tot_ff = active_burn_time / tot_t_irr    
+        tot_ff *= ff
     return tot_t_irr, tot_ff

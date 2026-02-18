231b3f89dfc6360d8837b810430bcb9f5241ed1b
Anupama Rajendra <asrajendra@wisc.edu>
2026-02-18 01:16:05 -0600

    calculate flux factor

--- tools/schedule_transforms.py
+++ tools/schedule_transforms.py
@@ -12,7 +12,6 @@ def flatten_pulse_history(pulse_length, num_pulses, dwell_time):
     :param num_pulses: (int) the number of pulses
     :param dwell_time: (float) the duration of the gap between each pulse
     """
-    
     t_irr = (num_pulses-1) * (pulse_length + dwell_time) + pulse_length
     flux_factor = num_pulses * pulse_length / t_irr
 
@@ -27,7 +26,10 @@ def flatten_ph_levels(pulse_length, nums_pulses, dwell_times):
     :param nums_pulses: (iterable) number of pulses at each level
     :param dwell_times: (iterable) the duration of the gap between each pulse at each level
     '''
+    active_burn_time = nums_pulses[0] * pulse_length
     tot_t_irr, ff = flatten_pulse_history(pulse_length, nums_pulses[0], dwell_times[0])
     for num_pulses, dwell_time in zip(nums_pulses[1:], dwell_times[1:]):
         tot_t_irr, ff = flatten_pulse_history(tot_t_irr, num_pulses, dwell_time)
-    return tot_t_irr
+        active_burn_time = active_burn_time * num_pulses
+    tot_ff = active_burn_time / tot_t_irr    
+    return tot_t_irr, tot_ff
--- tools/test_schedule_transforms.py
+++ tools/test_schedule_transforms.py
@@ -1,26 +1,28 @@
 import pytest
 import schedule_transforms as st
 
-# @pytest.mark.parametrize( "pulse_length,num_pulses,dwell_time,exp_tirr,exp_ff",
-#                           [
-#                             (1, 1, 1, 1, 1),
-#                             (1, 2, 1, 3, 2/3),
-#                             (2, 4, 6, 26, 8/26)                              
-#                           ])
-# def test_single_pulse_history(pulse_length, num_pulses, dwell_time, exp_tirr, exp_ff):
-#     obs_tirr, obs_ff = st.flatten_pulse_history(pulse_length, num_pulses, dwell_time)
-
-#     assert obs_tirr == exp_tirr
-#     assert obs_ff == exp_ff
-
-@pytest.mark.parametrize( "pulse_length,nums_pulses,dwell_times,exp_tot_tirr",
+@pytest.mark.parametrize( "pulse_length,num_pulses,dwell_time,exp_tirr,exp_ff",
                           [
-                            (1, [1,1], [1,1], 1),
-                            (1, [2,2], [1,2], 8),
-                            (2, [1,2], [2,2], 6)                              
+                            (1, 1, 1, 1, 1),
+                            (1, 2, 1, 3, 2/3),
+                            (2, 4, 6, 26, 8/26)                              
                           ])
-def test_flatten_ph_levels(pulse_length, nums_pulses, dwell_times, exp_tot_tirr):
-    obs_tot_tirr = st.flatten_ph_levels(pulse_length, nums_pulses, dwell_times)
+def test_single_pulse_history(pulse_length, num_pulses, dwell_time, exp_tirr, exp_ff):
+    obs_tirr, obs_ff = st.flatten_pulse_history(pulse_length, num_pulses, dwell_time)
+
+    assert obs_tirr == exp_tirr
+    assert obs_ff == exp_ff
+
+@pytest.mark.parametrize( "pulse_length,nums_pulses,dwell_times,exp_tot_tirr,exp_tot_ff",
+                          [
+                            (1, [1,1], [1,1], 1, 1),
+                            (1, [2,2], [1,2], 8, 4/8),
+                            (2, [1,2], [2,2], 6, 4/6)                              
+                          ])
+def test_flatten_ph_levels(pulse_length, nums_pulses, dwell_times, exp_tot_tirr, exp_tot_ff):
+    obs_tot_tirr, obs_tot_ff = st.flatten_ph_levels(pulse_length, nums_pulses, dwell_times)
+
     assert obs_tot_tirr == exp_tot_tirr
+    assert obs_tot_ff == exp_tot_ff
 
 

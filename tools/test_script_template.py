45323ad3196c8340a738f652ea816e50315d9581
Anupama Rajendra <asrajendra@wisc.edu>
2026-05-08 12:07:11 -0500

    remove returned value from test

--- tools/test_script_template.py
+++ tools/test_script_template.py
@@ -5,7 +5,6 @@ def test_modify_adf_for_db(adf):
     '''
     Ensure that the expected columns exist in the adf.
     '''
-    adf = script_temp.modify_adf_for_db(adf)
     assert not any(col in adf for col in [
         "value",
         "time",
@@ -17,7 +16,6 @@ def test_modify_adf_for_db(adf):
     ])
     assert (all(col in adf
                 for col in ["num_dens_(atoms/cm3)", "half_life", "block_name"]))
-    return adf
     
 def test_write_to_sqlite(adf, cursor):
     cursor.execute("SELECT * FROM number_densities")
@@ -38,9 +36,13 @@ def main():
         "nuclide": ["h-1"]*2,
         "half_life": ["-1"]*2,
         "run_lbl": ["test_case", "new_test_case"]})
-    adf = test_modify_adf_for_db(adf)
+    
+    adf = script_temp.modify_adf_for_db(adf)
+    test_modify_adf_for_db(adf)
+
     cursor = script_temp.write_to_sqlite(adf, db_name = ":memory:")
     test_write_to_sqlite(adf, cursor)
+
     script_temp.close_sqlite_conn(cursor) #this function has a built-in test in the form of catching any operational errors
 
 if __name__ == "__main__":

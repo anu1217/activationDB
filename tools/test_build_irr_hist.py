2cf403fc66d3135d35081e3d792b420e2084dfd8
Anupama Rajendra <asrajendra@wisc.edu>
2026-05-15 17:13:17 -0500

    add test for make_flux_dict()

--- tools/test_build_irr_hist.py
+++ tools/test_build_irr_hist.py
@@ -7,7 +7,7 @@ def normalize_lines(lines):
     '''
     return [" ".join(line.split()) for line in lines.splitlines() if line.strip()]
 
-@pytest.mark.parametrize("child_dicts, counter, exp_ph_dict", [
+@pytest.mark.parametrize("child_dicts, ph_counter, exp_ph_dict", [
     ([{
         'type':
         'schedule',
@@ -50,9 +50,57 @@ def normalize_lines(lines):
     })
 ])
 
-def test_make_ph_dict(child_dicts, counter, exp_ph_dict):
-    obs_ph_dict = build_irr_hist.make_ph_dict(child_dicts, counter)
+def test_make_ph_dict(child_dicts, ph_counter, exp_ph_dict):
+    obs_ph_dict = build_irr_hist.make_ph_dict(child_dicts, ph_counter)
     assert obs_ph_dict == exp_ph_dict
+
+@pytest.mark.parametrize("child_dicts, flux_counter, exp_flux_dict", [
+    ([{
+        'type':
+        'schedule',
+        'children': [
+            {
+                'type': 'pulse_entry',
+                'pulse_length': 7.6,
+                'pulse_length_unit': 'm',
+                'flux_filepath': './ex_flux',
+                'pulse_history': [(3, 7.9, 'm'), (2, 5.5, 's'), (9, 1.2, 'c')],
+                'delay_dur': 5.1,
+                'delay_dur_unit': 's'
+            }, {
+                'type': 'pulse_entry',
+                'pulse_length': 7.6,
+                'pulse_length_unit': 's',
+                'flux_filepath': '../flux_file',
+                'pulse_history': [(1, 8.0, 'm'), (2, 3, 's'), (9, 1.1, 'c')],
+                'delay_dur': 5.8,
+                'delay_dur_unit': 'm'
+            }
+        ],
+        'pulse_history': [(7, 9.5, 'd'), (3, 2.3, 'y')],
+        'delay_dur':
+        6.3,
+        'delay_dur_unit':
+        'm'
+    }, {
+        'type': 'pulse_entry',
+        'pulse_length': 7.4,
+        'pulse_length_unit': 'd',
+        'flux_filepath': '../flux_file',
+        'pulse_history': [(3, 7.9, 'm'), (2, 5.5, 's'), (9, 1.2, 'c')],
+        'delay_dur': 5.33,
+        'delay_dur_unit': 'c'
+    }],
+    None,
+    {
+        './ex_flux' : 'flux_1',
+        '../flux_file' : 'flux_3'
+    })
+    ])
+
+def test_make_flux_dict(child_dicts, flux_counter, exp_flux_dict):
+    obs_flux_dict = build_irr_hist.make_flux_dict(child_dicts, flux_counter)
+    assert obs_flux_dict == exp_flux_dict    
 
 @pytest.mark.parametrize("ph_dict, exp_ph_block", [
     ({
@@ -80,7 +128,7 @@ def test_make_pulse_history_block(ph_dict, exp_ph_block):
     obs_ph_block = build_irr_hist.make_pulse_history_block(ph_dict)
     assert normalize_lines(obs_ph_block) == normalize_lines(exp_ph_block)
 
-@pytest.mark.parametrize("child_dicts, ph_dict, flux_dict, counter, sched_name, exp_sched_block", [
+@pytest.mark.parametrize("child_dicts, ph_dict, flux_dict, sched_counter, sched_name, exp_sched_block", [
     ([{
         'type':
         'schedule',
@@ -137,8 +185,10 @@ def test_make_pulse_history_block(ph_dict, exp_ph_block):
         7.6	m	flux_1	pulse_history_2	5.1	s
         7.6	s	flux_3	pulse_history_3	5.8	m
     end""")
-    ])    
+    ])
 
-def test_make_schedule_block(child_dicts, ph_dict, flux_dict, counter, sched_name, exp_sched_block):
-    obs_sched_block = build_irr_hist.make_schedule_block(child_dicts, ph_dict, flux_dict, counter, sched_name)
+def test_make_schedule_block(child_dicts, ph_dict, flux_dict, sched_counter,
+                             sched_name, exp_sched_block):
+    obs_sched_block = build_irr_hist.make_schedule_block(
+        child_dicts, ph_dict, flux_dict, sched_counter, sched_name)
     assert normalize_lines(obs_sched_block) == normalize_lines(exp_sched_block)

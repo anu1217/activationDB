65ee23a39713c23a8f72069ebb44abbc36ffc70a
Anupama Rajendra <asrajendra@wisc.edu>
2026-05-19 12:55:35 -0500

    make actual pytests

--- tools/test_script_template.py
+++ tools/test_script_template.py
@@ -1,10 +1,29 @@
 import alara_output_processing as aop
 import script_template as script_temp
+import sqlite3
+import pytest
+
+@pytest.mark.parametrize( "adf",
+                          [
+                            (aop.ALARADFrame(data=
+        {"value": [5.678e-11]*2,
+        "time": [-1]*2,
+        "time_unit" : ["s"]*2,
+        "variable": [0]*2,
+        "var_unit": ["atoms/cm3"]*2,
+        "block" : [1,2],
+        "block_name": ["Be", "W"],
+        "block_num": [2]*2,
+        "nuclide": ["h-1"]*2,
+        "half_life": ["-1"]*2,
+        "run_lbl": ["test_case", "new_test_case"]}))
+                          ])
 
 def test_modify_adf_for_db(adf):
     '''
     Ensure that the expected columns exist in the adf.
     '''
+    adf = script_temp.modify_adf_for_db(adf)
     assert not any(col in adf for col in [
         "value",
         "time",
@@ -16,34 +35,27 @@ def test_modify_adf_for_db(adf):
     ])
     assert (all(col in adf
                 for col in ["num_dens_(atoms/cm3)", "half_life", "block_name"]))
-    
-def test_write_to_sqlite(adf, cursor):
-    cursor.execute("SELECT * FROM number_densities")
-    result = cursor.fetchall()
-    assert len(result) == len(adf["half_life"])
-    assert len(result[0]) == 6
 
-def main():
-    adf = aop.ALARADFrame(data=
-        {"value": [5.678e-11]*2,
-        "time": [-1]*2,
-        "time_unit" : ["s"]*2,
-        "variable": [0]*2,
-        "var_unit": ["atoms/cm3"]*2,
-        "block" : [1,2],
+@pytest.mark.parametrize( "mod_adf",
+                          [
+                            (aop.ALARADFrame(data=
+        {"num_dens_(atoms/cm3)": [5.678e-11]*2,
         "block_name": ["Be", "W"],
-        "block_num": [2]*2,
         "nuclide": ["h-1"]*2,
         "half_life": ["-1"]*2,
-        "run_lbl": ["test_case", "new_test_case"]})
+        "run_lbl": ["test_case", "new_test_case"]}))
+                          ])
     
-    adf = script_temp.modify_adf_for_db(adf)
-    test_modify_adf_for_db(adf)
+def test_write_to_sqlite(mod_adf):
+    cursor = script_temp.write_to_sqlite(mod_adf, db_name = ":memory:")
+    cursor.execute("SELECT * FROM number_densities")
+    result = cursor.fetchall()
+    assert len(result) == len(mod_adf["half_life"])
+    assert len(result[0]) == 6
 
-    cursor = script_temp.write_to_sqlite(adf, db_name = ":memory:")
-    test_write_to_sqlite(adf, cursor)
+@pytest.mark.parametrize( "test_cursor",
+                          [sqlite3.connect(":memory").cursor()])    
 
-    script_temp.close_sqlite_conn(cursor) #this function has a built-in test in the form of catching any operational errors
-
-if __name__ == "__main__":
-    main()
+def test_close_sqlite_conn(test_cursor):
+    # This function has a built-in test in the form of catching operational errors
+    script_temp.close_sqlite_conn(test_cursor)

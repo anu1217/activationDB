53a81cab41a7215003808644e4b5d7f721044277
Anupama Rajendra <asrajendra@wisc.edu>
2026-05-19 14:40:52 -0500

    add test for flux mapping

--- tools/test_script_template.py
+++ tools/test_script_template.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import alara_output_processing as aop
 import script_template as script_temp
 import sqlite3
@@ -56,6 +57,35 @@ def test_modify_adf_for_db(adf):
     ])
     assert (all(col in adf
                 for col in ["num_dens_(atoms/cm3)", "half_life", "block_name"]))
+
+@pytest.mark.parametrize( "test_adf, norm_flux_arr, t_irr_arr_mod, exp_mod_adf",
+                          [
+                            (aop.ALARADFrame(data=
+                            {"num_dens_(atoms/cm3)": [5.678e-11]*2,
+                            "block_name": ["Be", "W"],
+                            "nuclide": ["h-1"]*2,
+                            "half_life": ["-1"]*2,
+                            "run_lbl": ["test_case", "new_test_case"]}),
+                            np.array([[num / 25 for num in [1,3,5,7,9]], [num / 30 for num in [2,4,6,8,10]]
+                                    ]),
+                            np.array([7.5]*2),
+                            aop.ALARADFrame(data=
+                            {"num_dens_(atoms/cm3)": [5.678e-11]*2,
+                            "block_name": ["Be", "W"],
+                            "nuclide": ["h-1"]*2,
+                            "half_life": ["-1"]*2,
+                            "run_lbl": ["test_case", "new_test_case"],
+                            "flux_spec_shape" : [np.array([num / 25 for num in [1,3,5,7,9]]), 
+                                                 np.array([num / 30 for num in [2,4,6,8,10]])],
+                            "t_irr" : [7.5]*2
+                            })
+                            )
+                          ])
+
+def test_map_adf_flux_tirr(test_adf, norm_flux_arr, t_irr_arr_mod, exp_mod_adf):
+    obs_mod_adf = script_temp.map_adf_flux_tirr(test_adf, norm_flux_arr, t_irr_arr_mod)
+    assert pd.DataFrame(obs_mod_adf["flux_spec_shape"]).equals(pd.DataFrame(exp_mod_adf["flux_spec_shape"])) == True
+    assert all(obs_mod_adf["t_irr"]) == all(exp_mod_adf["t_irr"])
 
 @pytest.mark.parametrize( "mod_adf",
                           [

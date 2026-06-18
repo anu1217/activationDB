ff7be2348eb2fe32bd5560160fb3d114c522fd95
Anupama Rajendra <asrajendra@wisc.edu>
2026-06-18 11:39:51 -0500

    fix array initialization, delete unused code

--- tools/make_dwell_times.py
+++ tools/make_dwell_times.py
@@ -24,55 +24,6 @@ def calc_simple_ph_time_params(fluences, duty_cycles, nums_pulses):
     rel_dwell_times = (1 - duty_cycles) / duty_cycles
     abs_dwell_times = np.multiply.outer(rel_dwell_times, pulse_lengths)
     return pulse_lengths, abs_dwell_times
-     
-
-    
-# def calc_time_params_complex_ph(fluences, rel_dcs_lists, nums_pulses, calc_dwell_time):
-#     '''
-
-#     rel_dcs_lists and nums_pulses must have the same shape (chosen number x number of levels in ph)
-#     fluences has an arbitrary length
-#     pulse_lengths: 2D numpy array of shape (len(fluences), len(nums_pulses))
-#     '''
-#     accumulated_fluence = 0
-#     accumulated_off_time = 0
-#     all_dwell_times = np.array(len(rel_dcs_lists))
-#     pulse_lengths = np.array(len(rel_dcs_lists))
-#     for fluence in fluences:
-#         for rel_dcs_list_idx, rel_dc_list in enumerate(rel_dcs_lists):
-#             pulse_length = fluence / sum(nums_pulses[rel_dcs_list_idx])
-#             pulse_lengths[rel_dcs_list_idx] = pulse_length
-#             lvl_dwell_times = np.array(len(rel_dc_list))
-#             for rel_dc_idx, rel_dc in rel_dc_list:
-#                 #accumulated_off_time = accumulated_tot_dur - accumulated_fluence
-#                 lvl_dwell_times[rel_dc_idx] = calc_dwell_time(
-#                     accumulated_fluence=accumulated_fluence,
-#                     rel_dc = rel_dc,
-#                     pulse_length = pulse_length,
-#                     n_pulses = nums_pulses[rel_dcs_list_idx][rel_dc_idx],
-#                     accumulated_off_time=accumulated_off_time
-#                     )
-        
-#                 accumulated_off_time + accumulated_fluence, accumulated_fluence = st.flatten_ph_levels(pulse_length, list(zip((nums_pulses[rel_dcs_list_idx], lvl_dwell_times))))
-#             all_dwell_times[rel_dcs_list_idx] = lvl_dwell_times
-#     return pulse_lengths, all_dwell_times
-
-# def calc_time_params_complex_ph(fluences, rel_dcs_lists, nums_pulses, calc_dwell_time):
-#     '''
-
-#     rel_dcs_lists and nums_pulses must have the same shape (chosen number x number of levels in ph)
-#     fluences has an arbitrary length
-#     pulse_lengths: 2D numpy array of shape (len(fluences), len(nums_pulses))
-#     '''
-#     all_dwell_times = np.array(len(rel_dcs_lists))
-#     pulse_lengths = np.array(len(rel_dcs_lists))
-#     for fluence in fluences:
-#         for rel_dcs_list_idx, rel_dc_list in enumerate(rel_dcs_lists):
-#             pulse_length = fluence / sum(nums_pulses[rel_dcs_list_idx])
-#             pulse_lengths[rel_dcs_list_idx] = pulse_length
-#             lvl_dwell_times = accumulate_ph_fluence_off_time(rel_dc_list, pulse_length, nums_pulses[rel_dcs_list_idx], calc_dwell_time)
-#             all_dwell_times[rel_dcs_list_idx] = lvl_dwell_times
-#     return pulse_lengths, all_dwell_times
 
 def calc_time_params_complex_ph(fluences, rel_dcs_lists, nums_pulses, calc_dwell_time):
     '''
@@ -81,7 +32,7 @@ def calc_time_params_complex_ph(fluences, rel_dcs_lists, nums_pulses, calc_dwell
     fluences has an arbitrary length
     pulse_lengths: 2D numpy array of shape (len(fluences), len(nums_pulses))
     '''
-    all_dwell_times = np.array(len(rel_dcs_lists))
+    all_dwell_times = np.empty(len(rel_dcs_lists), dtype=object)
     pulse_lengths = np.outer(fluences, 1 / np.sum(rel_dcs_lists, axis=1)) #shape len(fluences), len(rel_dcs_lists)
     for fluence_idx, _ in enumerate(fluences):
         for rel_dcs_list_idx, rel_dc_list in enumerate(rel_dcs_lists):
@@ -94,7 +45,7 @@ def calc_time_params_complex_ph(fluences, rel_dcs_lists, nums_pulses, calc_dwell
 
 def accumulate_ph_fluence_off_time(rel_dc_list, pulse_length, nums_pulses, calc_dwell_time):
     accumulated_fluence = accumulated_off_time = 0
-    lvl_dwell_times = np.array(len(nums_pulses))
+    lvl_dwell_times = np.empty(len(nums_pulses))
     for rel_dc_idx, rel_dc in enumerate(rel_dc_list):
         lvl_dwell_times[rel_dc_idx] = calc_dwell_time(
                     accumulated_fluence=accumulated_fluence,

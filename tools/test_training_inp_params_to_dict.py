6da31355d2e1c1f5e3aba8a64eeb8c1da538de01
Anupama Rajendra <asrajendra@wisc.edu>
2026-06-24 17:41:00 -0500

    separate fluence calculation from populating dictionary

--- tools/test_training_inp_params_to_dict.py
+++ tools/test_training_inp_params_to_dict.py
@@ -2,12 +2,75 @@ import pytest
 import training_inp_params_to_dict
 import numpy as np
 
-@pytest.mark.parametrize("min_on_time, rel_on_time_factors, flux_norm_factors, flux_files, time_unit, exp_training_child_dicts", [
-    (1,
+
+@pytest.mark.parametrize("rel_on_time_factors, flux_norm_factors, flux_files, exp_training_inp_info", [
+    (
     np.array([2, 4]),
-     np.array([1, 0.3]),
-     np.array(['../iter_flux', '../frascati_flux']),
-     'y',
+    np.array([1, 0.3]),
+    np.array(['../iter_flux', '../frascati_flux']),
+    np.array([
+        [
+            [
+            None,
+            None
+            ],
+
+            [
+            (2, 0.3, '../iter_flux'),
+      
+      (2, 0.3, '../frascati_flux')
+            ]
+        ],
+
+        [
+            [
+            None,
+            None
+            ],
+
+            [
+            None,
+            None
+            ]
+        ]
+      ], dtype=object)
+     )
+     ])
+
+def test_make_flux_tirr_combos(rel_on_time_factors, flux_norm_factors, flux_files, exp_training_inp_info):
+    obs_training_inp_info = training_inp_params_to_dict.make_flux_tirr_combos(rel_on_time_factors, flux_norm_factors, flux_files)
+    assert np.array_equal(obs_training_inp_info, exp_training_inp_info)
+
+@pytest.mark.parametrize("training_inp_info, min_on_time, time_unit, exp_training_child_dicts", [
+    (
+    np.array([
+        [
+            [
+            None,
+            None
+            ],
+
+            [
+            (2, 0.3, '../iter_flux'),
+      
+      (2, 0.3, '../frascati_flux')
+            ]
+        ],
+
+        [
+            [
+            None,
+            None
+            ],
+
+            [
+            None,
+            None
+            ]
+        ]
+      ], dtype=object),
+    1,
+    'y',
     np.array([
         [
             [
@@ -51,6 +114,7 @@ import numpy as np
      )
      ])
 
-def test_write_training_params_dict(min_on_time, rel_on_time_factors, flux_norm_factors, flux_files, time_unit, exp_training_child_dicts):
-    obs_training_child_dicts = training_inp_params_to_dict.write_training_params_dict(min_on_time, rel_on_time_factors, flux_norm_factors, flux_files, time_unit)
+
+def test_write_training_params_dict(training_inp_info, min_on_time, time_unit, exp_training_child_dicts):
+    obs_training_child_dicts = training_inp_params_to_dict.write_training_params_dict(training_inp_info, min_on_time, time_unit)
     assert np.array_equal(obs_training_child_dicts, exp_training_child_dicts)
--- tools/training_inp_params_to_dict.py
+++ tools/training_inp_params_to_dict.py
@@ -2,50 +2,57 @@ import numpy as np
 from itertools import product
 
 
-def write_training_params_dict(min_on_time, rel_on_time_factors,
-                               flux_norm_factors, flux_files, time_unit):
+def make_flux_tirr_combos(rel_on_time_factors, flux_norm_factors, flux_files):
     """
-    This function takes a series of input parameters and converts them into a dictionary of the form below.
-    This dictionary can be used to build a single-line schedule with a single-line pulse history. A separate
-    dictionary for each viable combination of input parameters is constructed, and each dictionary is stored
-    in a numpy array of shape len(rel_on_time_factors) x len(flux_norm_factors) x len(flux_files).
+    This function calculates combinations of irradiation time and flux normalization factors
+    that do not exceed a prescribed value. The process is repeated for each flux spectrum.
+    The data is stored in a numpy array of shape len(rel_on_time_factors) x len(flux_norm_factors) x len(flux_files).
 
     The total amount of fluence is defined by the product of the minimum on-time
     and the maximum flux magnitude. If a combination of on-time and flux scaling factors results in
-    the total fluence being exceeded, the corresponding entry in the numpy array of dictionaries is
+    the total fluence being exceeded, the corresponding entry in the numpy array of input information is
     set to None.
-    [
-    {'type': 'pulse_entry',
-        'pulse_length': (float),
-        'pulse_length_unit': (str),
-        'flux_filepath' : (str),
-        'flux_norm' : (float),
-        'pulse_history': (iterable of (int, float, str)),
-        'delay_dur' : (float),
-        'delay_dur_unit': (str)
-    }
-    ]
-    :param: min_on_time (minimum total amount of time (float) during which the flux is nonzero)
+
     :param: rel_on_time_factors (iterable of factors (float) that scale the minimum on-time)
     :param: flux_norm_factors (iterable of factors (float) that scale the maximum flux magnitude)
     :param: flux_files (iterable of paths (str) to flux files)
-    :param: time_unit (unit (str) of pulse length)
     """
-    training_child_dicts = np.ndarray((len(rel_on_time_factors), len(flux_norm_factors), len(flux_files)), dtype=object)
+    training_inp_info = np.ndarray((len(rel_on_time_factors), len(flux_norm_factors), len(flux_files)), dtype=object)
     for (rel_on_time_factor_idx, rel_on_time_factor), (flux_norm_factor_idx, flux_norm_factor), (flux_file_idx, flux_file) in product(
                 enumerate(rel_on_time_factors),
                 enumerate(flux_norm_factors),
                 enumerate(flux_files)):
         if rel_on_time_factor * flux_norm_factor > 1:
-            training_child_dicts[rel_on_time_factor_idx, flux_norm_factor_idx, flux_file_idx] = None
+            training_inp_info[rel_on_time_factor_idx, flux_norm_factor_idx, flux_file_idx] = None
         else:
-            training_child_dicts[rel_on_time_factor_idx, flux_norm_factor_idx, flux_file_idx] = {
-                                        'type': 'pulse_entry',
-                                        'pulse_length': min_on_time * rel_on_time_factor,
-                                        'pulse_length_unit': time_unit,
-                                        'flux_filepath': flux_file,
-                                        'flux_norm': flux_norm_factor,
-                                        'pulse_history': [(1, 0, 's')],
-                                        'delay_dur': 0.0,
-                                        'delay_dur_unit': 's'}
-    return training_child_dicts
+            training_inp_info[rel_on_time_factor_idx, flux_norm_factor_idx, flux_file_idx] = (rel_on_time_factor, flux_norm_factor, flux_file)
+    return training_inp_info
+
+
+def write_training_params_dict(training_inp_info, min_on_time, time_unit):
+    """
+    This function takes a an array of input parameters and converts them into a dictionary of the form below.
+    This dictionary can be used to build a single-line schedule with a single-line pulse history. A separate
+    dictionary for each viable combination of input parameters is constructed, and each dictionary is stored
+    in a numpy array of shape len(rel_on_time_factors) x len(flux_norm_factors) x len(flux_files).
+
+    :param: min_on_time (minimum total amount of time (float) during which the flux is nonzero)
+    :param: time_unit (unit (str) of pulse length)
+    """
+    training_child_dicts = np.empty_like(training_inp_info)
+    for inp_info_idx in np.ndindex(training_inp_info.shape):
+        if training_inp_info[inp_info_idx] == None:
+            continue
+        else:
+            rel_on_time_factor, flux_norm_factor, flux_file = training_inp_info[inp_info_idx]
+            training_child_dicts[inp_info_idx] = {
+                        'type': 'pulse_entry',
+                        'pulse_length': min_on_time * rel_on_time_factor,
+                        'pulse_length_unit': time_unit,
+                        'flux_filepath': flux_file,
+                        'flux_norm': flux_norm_factor,
+                        'pulse_history': [(1, 0, 's')],
+                        'delay_dur': 0.0,
+                        'delay_dur_unit': 's'
+                        }
+    return training_child_dicts    

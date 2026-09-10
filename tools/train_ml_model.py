ec52d6aba776e5fd248c7d2ef84faae54ec2c213
Anupama Rajendra <asrajendra@wisc.edu>
2026-09-09 23:46:26 -0500

    change order of outputs

--- tools/train_ml_model.py
+++ tools/train_ml_model.py
@@ -194,7 +194,7 @@ def main():
 
     conn = sqlite3.connect(db_name)
     training_df = create_adf.make_pdf_from_sql(conn, filter_str)
-    Y_train, X_train = prepare_sql_adf_for_ml_model.make_training_features_outputs(training_df, ordered_nucs)
+    X_train, Y_train = prepare_sql_adf_for_ml_model.make_training_features_outputs(training_df, ordered_nucs)
     Y_train_flat = Y_train.reshape(Y_train.shape[0], -1)
 
     results = optimize_estimator_hyperparams(X_train, Y_train_flat)

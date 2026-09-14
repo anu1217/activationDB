65347c031c2dfc4ef3d51bd9cf4d20a6b9acd42e
Anupama Rajendra <asrajendra@wisc.edu>
2026-09-14 16:42:37 -0500

    separate child and parent nucs

--- tools/extract_sql_data.py
+++ tools/extract_sql_data.py
@@ -15,7 +15,7 @@ def make_dict_from_small_table(conn):
     dict_df = zip(dict_df['flux_spec_shape_id'], dict_df['flux_spec_shape'])
     return dict_df
 
-def make_df_from_num_dens_table(conn, filter_str, batch_size, ordered_nucs):
+def make_df_from_num_dens_table(conn, filter_str, batch_size, child_nucs, parent_nucs):
     query = """
     SELECT nuclide, run_lbl, block_name, [num_dens_(atoms/cm3)], flux_spec_shape_id, t_irr, avg_flux_mag
     FROM number_densities
@@ -28,10 +28,9 @@ def make_df_from_num_dens_table(conn, filter_str, batch_size, ordered_nucs):
                                    iter_batches = True,
                                    batch_size = batch_size,
                                    execute_options={"parameters":(filter_str,)},
-                                   schema_overrides={'nuclide' : pl.Enum([ordered_nuc.replace(":", "-") for ordered_nuc in ordered_nucs]
-                                                                         ),
+                                   schema_overrides={'nuclide' : pl.Enum(child_nucs),
                                                      'run_lbl' : pl.Categorical,
-                                                     'block_name' : pl.Enum(ordered_nucs),
+                                                     'block_name' : pl.Enum(parent_nucs),
                                                     'num_dens_(atoms/cm3)' : pl.Float32,
                                                     'flux_spec_shape_id' : pl.UInt8,
                                                     't_irr' : pl.Float32,

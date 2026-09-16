7e20d1f18da8c00afc0612e5967c9384acf4d005
Anupama Rajendra <asrajendra@wisc.edu>
2026-09-16 15:25:35 -0500

    close sqlite connection earlier

--- tools/train_ml_model.py
+++ tools/train_ml_model.py
@@ -209,9 +209,9 @@ def main():
         partial_training_df = extract_sql_data.make_df_from_num_dens_table(conn, filter_str, batch_size, child_nucs, parent_nucs)
         partial_training_df = pl.concat([df for df in partial_training_df])
         partial_training_df.write_csv("selected_df.csv")
+    conn.close()    
     X_train, Y_train = prepare_sql_adf_for_ml_model.make_training_features_outputs(partial_training_df, dict_df, child_nucs, parent_nucs)
     Y_train_flat = Y_train.reshape(Y_train.shape[0], -1)
-    conn.close()
     results = optimize_estimator_hyperparams(X_train, Y_train_flat)
     save_optimized_model(results)
 
